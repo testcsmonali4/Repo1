11b71f95bd5c5d4030521e0fe0fe163e673c0738
Cristhian Recalde <cristhian.recalde@aurea.com>
2018-05-05 11:08:55 -0400

    4 lines removed

--- src/com/devfactory/FirstClass.java
+++ src/com/devfactory/FirstClass.java
@@ -2,10 +2,6 @@ package com.devfactory;
 
 public class FirstClass {
 
-    public void testFirst() {
-        System.out.print("Nothing to do here");
-    }
-
     public void testSecond() {
         System.out.print("Nothing to do here again");
     }
