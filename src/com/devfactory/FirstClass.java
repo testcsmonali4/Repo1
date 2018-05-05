a4b74ef0345711826912efddfbfa98bb261dd73b
Cristhian Recalde <cristhian.recalde@aurea.com>
2018-05-05 16:32:51 -0400

    2 lines removed, 1 changed

--- src/com/devfactory/FirstClass.java
+++ src/com/devfactory/FirstClass.java
@@ -3,7 +3,7 @@ package com.devfactory;
 public class FirstClass {
 
     public void testSecond() {
-        System.out.print("Nothing to do here again");
+        System.out.print("Nothing to do anymore");
     }
 
 }
--- src/com/devfactory/SecondClass.java
+++ src/com/devfactory/SecondClass.java
@@ -2,8 +2,6 @@ package com.devfactory;
 
 public abstract class SecondClass {
 
-    abstract void methodOne();
-
     abstract void methodTwo();
 
 }
