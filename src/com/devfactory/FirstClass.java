290398dea3f32dd72cd01d78a1b2feb6e0ab87f4
Cristhian Recalde <cristhian.recalde@aurea.com>
2018-05-05 11:07:19 -0400

    14 lines added

---
+++ src/com/devfactory/FirstClass.java
@@ -0,0 +1,13 @@
+package com.devfactory;
+
+public class FirstClass {
+
+    public void testFirst() {
+        System.out.print("Nothing to do here");
+    }
+
+    public void testSecond() {
+        System.out.print("Nothing to do here again");
+    }
+
+}
